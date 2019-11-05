cd26fe3d3c058664357abc575cf3eb8c86748753
declarative-blog-8005 <anna@annajcarey.com>
2019-11-05 00:38:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,8 @@
 def reverse_each_word(sentence)
   new_array = sentence.split(" ")
-  new_array.each do |word|
+  new_array.collect do |word|
 	word.reverse!
 	end
 	return new_array.join(" ")
 end 
 
-def reverse_each_word(sentence)
-  new_array = sentence.split(" ")
-  new_array.map do |word|
-  word.reverse!
-end 
-return new_array.join(" ")
-end 
